75f82eae94ace7e5d80b7428f9b468edd3f5db0b
masaaki-todoroki <code.groove.me.dev@gmail.com>
2023-08-06 10:43:09 +0900

    [fix] Modified codegen to work

--- lib/codegen.ts
+++ lib/codegen.ts
@@ -1,10 +1,17 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+dotenv.config();
 
-const HASURA_GRAPHQL_ENDPOINT = process.env.HASURA_ENDPOINT;
+const HASURA_GRAPHQL_ENDPOINT = process.env.HASURA_ENDPOINT_FOR_CODEGEN;
 if (!HASURA_GRAPHQL_ENDPOINT) {
   throw new Error(
-    "The HASURA_GRAPHQL_ENDPOINT environment variable is not defined.",
+    "HASURA_GRAPHQL_ENDPOINT environment variable is not defined.",
   );
+}
+
+const HASURA_JWT_TOKEN = process.env.HASURA_JWT_TOKEN_FOR_CODEGEN;
+if (!HASURA_JWT_TOKEN) {
+  throw new Error("HASURA_JWT_TOKEN environment variable is not defined.");
 }
 
 const config: CodegenConfig = {
@@ -12,7 +19,7 @@ const config: CodegenConfig = {
   schema: {
     [HASURA_GRAPHQL_ENDPOINT]: {
       headers: {
-        Authorization: `Bearer ${process.env.HASURA_JWT_TOKEN}`,
+        Authorization: `Bearer ${HASURA_JWT_TOKEN}`,
       },
     },
   },
