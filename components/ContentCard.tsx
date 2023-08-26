5385798f80e9f32597b8e9dde2a6666a5f4de2d8
masaaki-todoroki <code.groove.me.dev@gmail.com>
2023-08-26 18:05:58 +0900

    [refactor] sx instead of className

--- components/ContentCard.tsx
+++ components/ContentCard.tsx
@@ -1,20 +1,15 @@
 import { FC, ReactNode } from "react";
-import { Box, Paper, Title } from "@mantine/core";
+import { Box, Paper, Title, Sx } from "@mantine/core";
 
 type Props = {
   children: ReactNode;
   title: string;
-  className?: string;
   outerTitle?: boolean;
+  sx?: Sx;
 };
 
-export const ContentCard: FC<Props> = ({
-  children,
-  title,
-  className,
-  outerTitle
-}) => (
-  <Box className={className}>
+export const ContentCard: FC<Props> = ({ children, title, outerTitle, sx }) => (
+  <Box sx={sx}>
     {outerTitle && (
       <Title order={2} mb="sm">
         {title}
